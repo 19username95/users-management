3553064660aed14ad3e7c39459eebcfebc439795
unknown <kazakovayana@outlook.com>
2020-11-06 18:26:04 +0200

    draw users from state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,12 +13,53 @@ import UsersList from "../UsersList/UsersList";
 import About from "../About/About";
 
 class App extends React.Component {
+    state = {
+        users: [
+            {
+                id: 0,
+                name: "username",
+                surname: "usernamov",
+                sex: "male",
+                card: "0000 0000 0000 0000",
+                isLoyalty: false,
+                coupon: "",
+                registrationDate: ""
+            },
+            {
+                id: 2,
+                name: "useriha",
+                surname: "usernamova",
+                sex: "female",
+                card: "0000 0000 0000 0000",
+                isLoyalty: false,
+                coupon: "",
+                registrationDate: ""
+            },
+        ],
+    }
+
+    /*
+    componentDidMount() {
+        fetch('../mocks/all-users.json')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        users: result.users
+                    });
+                }
+            ).then(this.setState)
+    }
+*/
+
     render() {
         return (
             <Router>
                 <Navigation />
                 <Switch>
-                    <Route path='/users-list'><UsersList /></Route>
+                    <Route path='/users-list'>
+                        <UsersList users={this.state.users} />
+                    </Route>
                     <Route path='/add-user'><Registration /></Route>
                     <Route path='/about'><About /></Route>
                     <Route path='/'>
