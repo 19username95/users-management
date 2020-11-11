c7b3ed3ce69be3e8e965178c584e73e754664624
unknown <kazakovayana@outlook.com>
2020-11-11 17:21:44 +0200

    add something

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import './About.scss';
+import {Avatar, Box} from "@material-ui/core";
+import avatar from "../../assets/images/avatar.png";
+import {Email, GitHub, LinkedIn, Phone, Telegram} from "@material-ui/icons";
+import Title from "../Title/Title";
+import { Link } from "@material-ui/core";
 
 class About extends React.Component {
     render() {
-        return <div>hello from about</div>
+        return <Box className='About'>
+            <Avatar className='About-Avatar' alt='Avatar' src={avatar} />
+            <div className='About-Name'>Jana Kazakova</div>
+
+            <div className='About-Contacts'>
+                <Title className='About-ContactsTitle'>Contacts</Title>
+                <div className='About-Contact'><Email/> kazakovayana95@icloud.com</div>
+                <div className='About-Contact'><Phone/> +38 (063) 807-11-38</div>
+                <Link className='About-ContactLink' href='https://github.com/19username95/'>
+                    <GitHub/>
+                </Link>
+                <Link className='About-ContactLink' href='https://www.t.me/kazakovayana95/'>
+                    <Telegram/>
+                </Link>
+                <Link className='About-ContactLink' href='https://www.linkedin.com/in/jana-kazakova-81571419a/'>
+                    <LinkedIn/>
+                </Link>
+            </div>
+        </Box>
     }
 }
 
