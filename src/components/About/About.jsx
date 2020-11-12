1f677f157cbd87da5bfce735e71e4f8ffb2b44bc
unknown <kazakovayana@outlook.com>
2020-11-12 18:31:46 +0200

    enough for desktop

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './About.scss';
 import {Avatar, Box} from "@material-ui/core";
 import avatar from "../../assets/images/avatar.png";
-import {Email, GitHub, LinkedIn, Phone, Telegram} from "@material-ui/icons";
+import {Email, GitHub, LinkedIn, LocationOn, Phone, Telegram} from "@material-ui/icons";
 import Title from "../Title/Title";
 import { Link } from "@material-ui/core";
 import Skill from "../Skill/Skill";
 import Institution from "../Institution/Institution";
 import Work from "../Work/Work";
+
+//const _star=`\u2B50`
 
 class About extends React.Component {
     render() {
@@ -27,6 +29,45 @@ class About extends React.Component {
                 <Link className='About-ContactLink' href='https://www.linkedin.com/in/jana-kazakova-81571419a/'>
                     <LinkedIn/>
                 </Link>
+            </div>
+            <div className='About-SoftInfo'>
+                <div className='About-Location'>
+                    <Title>Location</Title>
+                    <div className='About-Address'>
+                        <LocationOn className='About-Address_icon'/> Dnipro, Ukraine
+                    </div>
+                </div>
+                <div className='About-Languages'>
+                    <Title>Languages</Title>
+                    <div><b>Russian:</b> Native</div>
+                    <div><b>English:</b> Intermediate</div>
+                </div>
+                <div className='About-Interests'>
+                    <Title>Interests</Title>
+                    <ul className='About-InterestsList'>
+                        <li className='About-InterestsListItem'>Traveling</li>
+                        <li className='About-InterestsListItem'>Skiing</li>
+                        <li className='About-InterestsListItem'>Cooking & baking</li>
+                        <li className='About-InterestsListItem'>Board games</li>
+                        <li className='About-InterestsListItem'>Movies, computer games</li>
+                        <li className='About-InterestsListItem'>Hookahs</li>
+                    </ul>
+                </div>
+                <div className='About-Certificates'>
+                    <Title>Certificates</Title>
+                    <Institution name='Private Bank Certification'
+                                 faculty='Java Junior'
+                                 specialty='University internship'
+                                 year='2020' />
+                    <Institution name={'CISCO Academy'}
+                                 faculty={'Cybersecurity I'}
+                                 year={'2020'}
+                    />
+                    <Institution name={'CISCO Academy'}
+                                 faculty={'CCNA I'}
+                                 year={'2019'}
+                    />
+                </div>
             </div>
             <div className='About-Skills'>
                 <Title>Skills</Title>
@@ -58,26 +99,15 @@ class About extends React.Component {
                              year={'2020'}
                              isUniversity={true}
                 />
-                <Institution className='About-Institution'
-                             name={'CISCO Academy'}
-                             faculty={'Cybersecurity I'}
-                             year={'2019'}
-                />
-                <Institution className='About-Institution'
-                             name={'CISCO Academy'}
-                             faculty={'CCNA I'}
-                             year={'2019'}
-                />
             </div>
             <div className='About-Experience'>
                 <Title>Experience</Title>
-                <Work title='Freelance author'
-                      company='Self employeed'
-                      location={'Remote'}
-                      startDate='09.2018'
+                <Work title='Intern software engineer'
+                      company='HeadWorks'
+                      location='Dnipro, Ukraine'
+                      startDate='10.20'
                       finishDate='present'
-                      about='I write lab works, courseworks, graduation projects (diplomas), small projects using various
-                      platforms: Assembler, .NET, Python, JS, GPSS (systems simulation programming language), etc.'/>
+                      about='I do internship as ReactJS intern.' />
                 <Work title='Software engineer'
                       company='Mive'
                       location='Dnipro, Ukraine'
@@ -86,12 +116,13 @@ class About extends React.Component {
                       about='It is a small company, specializing on software development.
                       I used Xamarin.Forms, Angular 8, WPF, ASP.NET and other technologies for develop
                       software products.'/>
-                <Work title='Intern software engineer'
-                        company='HeadWorks'
-                        location='Dnipro, Ukraine'
-                        startDate='10.20'
-                        finishDate='present'
-                        about='I do internship as ReactJS intern.' />
+                <Work title='Freelance author'
+                      company='Self employeed'
+                      location={'Remote'}
+                      startDate='09.2018'
+                      finishDate='present'
+                      about='I write lab works, courseworks, graduation projects (diplomas), small projects using various
+                      platforms: Assembler, .NET, Python, JS, GPSS (systems simulation programming language), etc.'/>
             </div>
         </Box>
     }
