4dff684b523834eba5360b5dacc4cc7df0f76c66
unknown <kazakovayana@outlook.com>
2020-11-12 19:59:03 +0200

    mobile

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,18 +17,22 @@ class About extends React.Component {
             <Avatar className='About-Avatar' alt='Avatar' src={avatar} />
             <div className='About-Name'>Jana Kazakova</div>
             <div className='About-Contacts'>
-                <Title className='About-ContactsTitle'>Contacts</Title>
+                <div className="About-ContactsTitleContainer">
+                    <Title className='About-ContactsTitle'>Contacts</Title>
+                </div>
                 <div className='About-Contact'><Email/> kazakovayana95@icloud.com</div>
                 <div className='About-Contact'><Phone/> +38 (063) 807-11-38</div>
-                <Link className='About-ContactLink' href='https://github.com/19username95/'>
-                    <GitHub/>
-                </Link>
-                <Link className='About-ContactLink' href='https://www.t.me/kazakovayana95/'>
-                    <Telegram/>
-                </Link>
-                <Link className='About-ContactLink' href='https://www.linkedin.com/in/jana-kazakova-81571419a/'>
-                    <LinkedIn/>
-                </Link>
+                <div className='About-ContactLinks'>
+                    <Link className='About-ContactLink' href='https://github.com/19username95/'>
+                        <GitHub/>
+                    </Link>
+                    <Link className='About-ContactLink' href='https://www.t.me/kazakovayana95/'>
+                        <Telegram/>
+                    </Link>
+                    <Link className='About-ContactLink' href='https://www.linkedin.com/in/jana-kazakova-81571419a/'>
+                        <LinkedIn/>
+                    </Link>
+                </div>
             </div>
             <div className='About-SoftInfo'>
                 <div className='About-Location'>
